dd34367ca432688108e730e92e17e5c6fc8892dd
z-felixhan <34609664+z-felixhan@users.noreply.github.com>
2018-06-30 02:05:38 -0400

    Took out Bootstrap and added Materialize instead. Updated front-end aesthetics

--- todos/forms.py
+++ todos/forms.py
@@ -4,6 +4,8 @@ class TodoForm(forms.Form):
     text = forms.CharField(max_length=40,
         widget=forms.TextInput(
             attrs={
+                'class': 'center-align validate',
+                'data-length': '40',
                 'placeholder': 'Enter task'
             }
         ))
